3dc2f5c924dd370e79b4f8dde50d33ed60479cca
haseeb1511 <haseebmanzoor1511@gmail.com>
2025-06-25 20:15:12 +0500

    cnn

--- clearn_notebook.py
+++ clearn_notebook.py
@@ -1,8 +1,9 @@
 import nbformat
+
 # pip install nbformat nbconvert
 
 # Load the notebook with explicit UTF-8 encoding
-notebook_path = 'Rag_with_weviate.ipynb'
+notebook_path = 'T5.ipynb'
 with open(notebook_path, 'r', encoding='utf-8') as file:
     notebook = nbformat.read(file, as_version=4)
 
@@ -11,5 +12,5 @@ if 'metadata' in notebook:
     notebook['metadata'].pop('widgets', None)
 
 # Save the cleaned notebook with UTF-8 encoding
-with open('Rag_with_weviate.ipynb', 'w', encoding='utf-8') as file:
+with open('T5_cleaned.ipynb', 'w', encoding='utf-8') as file:
     nbformat.write(notebook, file)
